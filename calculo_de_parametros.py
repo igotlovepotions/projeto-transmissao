4d52519bb314a610bdd3cda1d1fe5e3427a4a55a
Igotlovepotions <69840347+igotlovepotions@users.noreply.github.com>
2026-01-04 11:40:00 -0300

    bruh

---
+++ calculo_de_parametros.py
@@ -0,0 +1,9 @@
+from mostrar_cabos_grafico.py import cables
+
+# cables = {
+#     'A': {'altura': 28, 'distancia': -5},
+#     'B': {'altura': 34, 'distancia': 0},
+#     'C': {'altura': 28, 'distancia': 5},
+#     'PR1': {'altura': 39, 'distancia': 0},
+#    # 'PR2': {'altura': 39, 'distancia': 7},
+# }
